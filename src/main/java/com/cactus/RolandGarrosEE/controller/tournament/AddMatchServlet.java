8fb49145371775d08000d10d4682614eaf5f553a
Arnaud Saulou <a.saulou@gmail.com>
2020-12-15 23:24:07 +0100

    BugFix

--- src/main/java/com/cactus/RolandGarrosEE/controller/tournament/AddMatchServlet.java
+++ src/main/java/com/cactus/RolandGarrosEE/controller/tournament/AddMatchServlet.java
@@ -13,6 +13,8 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.*;
 
 @WebServlet(name = "ajouterMatch", value = "/tournoi/ajouterMatch")
